c48a580717bf86580e237e4a1a0d2880d09d5892
Eduardo Sanches Bocato <dubocato@gmail.com>
2019-01-04 22:22:21 -0200

    Initial Commit

---
+++ Networking/Networking.h
@@ -0,0 +1,19 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Eduardo Bocato on 04/01/19.
+//  Copyright © 2019 Eduardo Bocato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Networking.
+FOUNDATION_EXPORT double NetworkingVersionNumber;
+
+//! Project version string for Networking.
+FOUNDATION_EXPORT const unsigned char NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking/PublicHeader.h>
+
+
